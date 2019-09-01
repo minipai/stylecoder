215e87759b3f57a7a9d3b36f5c77987f00fed043
Art Pai <minipai@gmail.com>
2019-09-01 23:10:19 +1000

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Hi, I am Art Pai`,
+    author: `Art Pai`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `minpai`,
     },
   },
   plugins: [
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Hi, I am Art Pai`,
+        short_name: `Art Pai`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
